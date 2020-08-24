9c4a0a5d3b5ac9489242f1858be076f33e87f45b
suitepotato <69999133+suitepotato@users.noreply.github.com>
2020-08-24 12:20:24 +0900

    sep과 end 관련 예제 추가
    03강 README.md 파일에 첨부한 예제도 여기에 포함하였습니다.

--- Basics/03 - 기본 입출력 함수/builtin_IOs.py
+++ Basics/03 - 기본 입출력 함수/builtin_IOs.py
@@ -4,8 +4,10 @@ print(type(True))
 print(type("Hello World!"))
 print(type(3.14))
 
-"""
+# end 인자를 이용해 한 print 구문의 끝 부분에 어떤 문자열이 들어갈지, sep 인자를 이용해 데이터 사이 사이에 어떤 문자열이 들어갈지 정할 수 있어요.
+print("H", "e", "l", "l", "o", sep="", end=" ")
+print("Python")
+
 # 기본 입력함수 input입니다. input의 파라미터로는 콘솔에 출력할 문자열을 전달하며, input 함수는 콘솔을 통해 입력받은 데이터를 반환합니다.
 answer = input("정답을 입력해주세요! : ")
 print("입력하신 정답은", answer, "입니다.")
-"""
